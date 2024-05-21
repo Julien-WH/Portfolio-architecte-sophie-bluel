bc1847e06fb8bfe8dcfe99ac90b87254826e6ce9
Julien WH <wh.julien+github.com@gmail.com>
2024-05-21 21:47:54 +0200

    login check
    Used import and exports to check logging status

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,100 +1,59 @@
 const loginForm = document.getElementById('loginForm');
 const loginInput = document.getElementById('loginInput');
 const passwordInput = document.getElementById('passwordInput');
-const token = localStorage.getItem('token'); // Get the token from local storage
-export const tokenStatus = checkToken();
+import { tokenStatus } from './script.js';
 
-function checkToken() {
-    let tokenStatus = false;
+// Récupérer les données du formulaire de connexion et les envoyer à l'API
+if (tokenStatus === false) {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const login = loginInput.value;
+        const password = passwordInput.value;
+        const response = await fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: login,
+                password: password
+            })
+        });
+        const data = await response.json();
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            // Rediriger l'utilisateur vers la page d'accueil
+            window.location.href = 'index.html';
+        } else {
+            const errorMessage = document.createElement('div');
+            errorMessage.textContent = "Identifiants incorrects. Veuillez réessayer.";
+            errorMessage.classList.add('error');
+            loginForm.appendChild(errorMessage);
+        }
+})
+};
 
-    if (token) {
-        tokenStatus = true;
-    }
-    // console.log(tokenStatus);
-    return tokenStatus;
-}
-
-if (checkToken === true) {
-    // Display the connected message
+// Afficher le message de statut connecté
+if (tokenStatus === true) {
+    // Création des éléments HTML
     const connectedMessage = document.createElement('div');
     connectedMessage.textContent = 'Vous êtes connecté.e. ';
-
     const disconnectLink = document.createElement('a');
     disconnectLink.textContent = '[Se déconnecter]';
     disconnectLink.href = '#';
     disconnectLink.addEventListener('click', () => {
-        // Remove the token from local storage
+        // Supprimer le token du local storage
         localStorage.removeItem('token');
-        // Reload the page
+        // Recharger la page
         window.location.reload();
     });
 
+    // Ajout des éléments dans le DOM
     connectedMessage.appendChild(disconnectLink);
-
     const mainTag = document.querySelector('main');
-    const firstH2 = document.querySelector('h2');
-    mainTag.insertBefore(connectedMessage, firstH2.nextSibling);
+    const H2tag = document.querySelector('h2');
+    mainTag.insertBefore(connectedMessage, H2tag.nextSibling);
     loginForm.style.display = 'none';
 }
-
-
-// if (token) {
-//     // Display the connected message
-//     const connectedMessage = document.createElement('div');
-//     connectedMessage.textContent = 'Vous êtes connecté.e. ';
-
-//     const disconnectLink = document.createElement('a');
-//     disconnectLink.textContent = '[Se déconnecter]';
-//     disconnectLink.href = '#';
-//     disconnectLink.addEventListener('click', () => {
-//         // Remove the token from local storage
-//         localStorage.removeItem('token');
-//         // Reload the page
-//         window.location.reload();
-//     });
-
-//     connectedMessage.appendChild(disconnectLink);
-
-//     const mainTag = document.querySelector('main');
-//     const firstH2 = document.querySelector('h2');
-//     mainTag.insertBefore(connectedMessage, firstH2.nextSibling);
-//     loginForm.style.display = 'none';
-// }
-
-// loginForm.addEventListener('submit', (event) => {
-//     event.preventDefault();
-
-//     const login = loginInput.value;
-//     const password = passwordInput.value;
-
-//     fetch('http://localhost:5678/api/users/login', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             email: login,
-//             password: password
-//         })
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.token) {
-//                 // Store the token and userID in local storage
-//                 localStorage.setItem('userId', data.userId);
-//                 localStorage.setItem('token', data.token);
-//                 // Redirect to index.html
-//                 window.location.href = 'index.html';
-//             } else {
-//                 // Display an error message in the DOM
-//                 const errorMessage = document.createElement('div');
-//                 errorMessage.textContent = 'Invalid login credentials';
-
-//                 const mainTag = document.querySelector('main');
-//                 const firstH2 = document.querySelector('h2');
-//                 mainTag.insertBefore(errorMessage, firstH2.nextSibling);
-//             }
-//         })
-// });
-
 
